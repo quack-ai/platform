453c2a8290a73739eb257505c6c0753e1dccdd18
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-09-30 17:16:13 +0200

    feat: Updates font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,10 @@ export default function Document() {
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={metaDescription} />
         <meta name="twitter:image" content={metaBanner} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Onest:wght@400&family=Poppins:wght@300;400&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
